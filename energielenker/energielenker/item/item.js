1407b5698cf0d7ca170e3ee3b80f05346dfd48bb
Joelios <joel.mesmer@libracore.com>
2021-09-24 08:45:04 +0200

    hotfix Artikelanlage

--- energielenker/energielenker/item/item.js
+++ energielenker/energielenker/item/item.js
@@ -3,12 +3,14 @@
 
 frappe.ui.form.on("Item", {
     validate: function(frm) {
-        var supplier_items = cur_frm.doc.supplier_items;
-        var suchliste_list = [];
-        for (var i=0; i < supplier_items.length; i++) {
-            suchliste_list.push(supplier_items[i].supplier_part_no);
+        if (cur_frm.doc.supplier_items) {
+            var supplier_items = cur_frm.doc.supplier_items;
+            var suchliste_list = [];
+            for (var i=0; i < supplier_items.length; i++) {
+                suchliste_list.push(supplier_items[i].supplier_part_no);
+            }
+            var suchliste = suchliste_list.join();
+            frm.set_value("suchfeld", suchliste);
         }
-        var suchliste = suchliste_list.join();
-        frm.set_value("suchfeld", suchliste);
     }
 });
