a6fada3d11a70f1ccf1be330f8524aa4237746ea
Joelios <joel.mesmer@libracore.com>
2021-09-28 08:37:21 +0200

    address js fills value in line2, old field hidden, adjusted all print formats for phone instead mobile

--- energielenker/energielenker/address/address.js
+++ energielenker/energielenker/address/address.js
@@ -4,6 +4,11 @@ frappe.ui.form.on('Address', {
             cur_frm.set_value('pincode', cur_frm.doc.plz);
         } else {
             cur_frm.set_value('pincode', '');
-        }
+        },
+	if (cur_frm.doc.company_adr_line2) {
+	   cur_frm.set_value('address_line2', cur_frm.doc.company_adr_line2);
+	} else {
+	   cur_frm.set_value('address_line2', '');
+	}
     }
 })
