327e0db0447ec9cd859a4c66d6d8df06cabdd958
Joelios <joel.mesmer@libracore.com>
2021-11-04 15:13:00 +0100

    removed doctype navisionaccount

--- energielenker/energielenker/doctype/navisionaccount/navisionaccount.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2021, libracore and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Navisionaccount', {
-	// refresh: function(frm) {
-
-	// }
-});
