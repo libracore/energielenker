4301e5d97103b809c277b4ef33020b9a5f33023f
nanipi <info@libracore.ch>
2021-11-12 14:17:05 +0100

    changed naming series option in PO

--- energielenker/energielenker/doctype/quotation_template/quotation_template.js
+++ energielenker/energielenker/doctype/quotation_template/quotation_template.js
@@ -2,7 +2,9 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Quotation Template', {
-//
+    tc_name: function(frm) {
+        cur_frm.add_fetch('tc_name', 'terms', 'terms');
+    }
 });
 
 frappe.ui.form.on('Quotation Item', {
