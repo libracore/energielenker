a770d319ea0daf6c49fd8812908ca9b1295abfad
Joelios <joel.mesmer@libracore.com>
2021-12-19 21:15:34 +0100

    Empfänger automatische Wiederholung

--- energielenker/energielenker/auto_repeat/auto_repeat.js
+++ energielenker/energielenker/auto_repeat/auto_repeat.js
@@ -1,6 +1,8 @@
 frappe.ui.form.on('Auto Repeat', {
     validate: function(frm) {
         cur_frm.set_value('notify_by_email', 1);
-        cur_frm.set_value('recipients', frappe.session.user_email);
+        if (!cur_frm.doc.recipients) {
+            cur_frm.set_value('recipients', frappe.session.user_email);
+        }
     }
 })
