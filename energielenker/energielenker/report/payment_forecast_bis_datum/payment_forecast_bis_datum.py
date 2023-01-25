c511827f680d2ce831414eff69a44a35b2db077b
Joelios <joel.mesmer@libracore.com>
2023-01-25 11:40:40 +0100

    v2.98.0

--- energielenker/energielenker/report/payment_forecast_bis_datum/payment_forecast_bis_datum.py
+++ energielenker/energielenker/report/payment_forecast_bis_datum/payment_forecast_bis_datum.py
@@ -17,7 +17,8 @@ def get_columns():
         {"label": _("Projektname"), "fieldname": "project_name", "fieldtype": "Data", "width": 100},
         {"label": _("Kostenstelle"), "fieldname": "cost_center", "fieldtype": "Link", "options": "Cost Center", "width": 100},
         {"label": _("Zahlungsdatum bis einschl."), "fieldname": "due_date", "fieldtype": "Date", "width": 100},
-        {"label": _("Saldo offene Zahlungsbeträge"), "fieldname": "outstanding_amount", "fieldtype": "Currency"}
+        {"label": _("Saldo offene Zahlungsbeträge"), "fieldname": "outstanding_amount", "fieldtype": "Currency"},
+        {"label": _("Saldo Zahlungsbeträge > Forecast"), "fieldname": "over_amount", "fieldtype": "Currency"}
     ]
     return columns
 
@@ -37,35 +38,34 @@ def get_data(filters):
                             GROUP BY `parent`""".format(date=filters.date), as_dict=True)
     
     for order in orders:
-        project_payment_forecast = frappe.db.sql("""SELECT
-                                                        SUM(`pf`.`amount`) AS `outstanding_amount`,
-                                                        `pf`.`parent` AS `project`,
-                                                        `pf`.`order` AS `sales_order`,
-                                                        `p`.`project_name` AS `project_name`
-                                                    FROM `tabPayment Forecast` AS `pf`
-                                                    LEFT JOIN `tabProject` AS `p` ON `pf`.`parent` = `p`.`name`
-                                                    WHERE `pf`.`order` = '{sales_order}'
-                                                    AND `pf`.`date` <= '{date}'
-                                                    AND `pf`.`invoice_created` != 1
-                                                    GROUP BY `pf`.`order`""".format(sales_order=order.sales_order, date=filters.date), as_dict=True)
-        if len(project_payment_forecast) > 0:
-            project_payment_forecast = project_payment_forecast[0]
-            outstanding_amount = project_payment_forecast.outstanding_amount
+        gestellte_rechnungen = frappe.db.sql("""SELECT
+                                                    SUM(`amount`) AS `amount`
+                                                FROM `tabSales Order Anzahlung`
+                                                WHERE `parent` = '{0}'""".format(order.sales_order), as_dict=True)
+        
+        project = frappe.db.get_value('Sales Order', order.sales_order, 'project') or None
+        project_name = frappe.db.get_value('Project', project, 'project_name') or None if project else None
+        if len(gestellte_rechnungen) > 0:
+            if gestellte_rechnungen[0].amount:
+                gestellte_rechnungen_amount = float(gestellte_rechnungen[0].amount)
+            else:
+                gestellte_rechnungen_amount = 0
         else:
-            project = frappe.db.get_value('Sales Order', order.sales_order, 'project') or None
-            project_name = frappe.db.get_value('Project', project, 'project_name') or None if project else None
-            project_payment_forecast = False
-            outstanding_amount = (frappe.db.get_value('Sales Order', order.sales_order, 'grand_total') / 100) * (100 - frappe.db.get_value('Sales Order', order.sales_order, 'per_billed') or 0)
+            gestellte_rechnungen_amount = 0
+        
+        order_payment_amount = order.payment_amount if order.payment_amount > 0 else 0
+        outstanding_amount = order_payment_amount - gestellte_rechnungen_amount
         
         _data = {
             'sales_order': order.sales_order,
-            'project': project_payment_forecast.project if project_payment_forecast else project,
-            'project_name': project_payment_forecast.project_name if project_payment_forecast else project_name,
+            'project': project,
+            'project_name': project_name,
             'cost_center': frappe.db.get_value('Sales Order', order.sales_order, 'cost_center') or None,
             'due_date': order.due_date,
-            'outstanding_amount': outstanding_amount
+            'outstanding_amount': outstanding_amount if outstanding_amount > 0 else 0,
+            'over_amount': 0 if outstanding_amount > 0 else (outstanding_amount * -1)
         }
-        if outstanding_amount > 0:
+        if outstanding_amount > 0 or outstanding_amount < 0:
             data.append(_data)
         elif not filters.not_null:
             data.append(_data)
