e578ec54e556fd22b2e3d50a44b3d4bfa4f9e7b8
Joelios <joel.mesmer@libracore.com>
2023-02-17 15:57:24 +0100

    v2.107.0

--- energielenker/energielenker/issue/issue.py
+++ energielenker/energielenker/issue/issue.py
@@ -44,5 +44,6 @@ def check_for_assigment(self):
         frappe.db.commit()
 
 def mark_for_reply(self, event):
-    frappe.db.set_value("Issue", self.name, 'mark_for_reply', 1, update_modified=False)
-    frappe.db.commit()
+    if self.issue_type != 'Reklamation':
+        frappe.db.set_value("Issue", self.name, 'mark_for_reply', 1, update_modified=False)
+        frappe.db.commit()
