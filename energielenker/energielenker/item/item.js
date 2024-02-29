b97d67a3f34af71664e36e21a561a6e8a8a7487a
joelios <joel.mesmer@libracore.com>
2024-02-29 09:13:52 +0100

    create_labels

--- energielenker/energielenker/item/item.js
+++ energielenker/energielenker/item/item.js
@@ -97,20 +97,6 @@ function set_timestamps(frm){
 }
 
 function create_label(frm, label_printer) {
-    // html-content of the label
-    var dimensions = get_dimensions(label_printer);
-    var content = get_label_content(frm, dimensions);
-    var w = window.open(
-         frappe.urllib.get_full_url("/api/method/erpnextswiss.erpnextswiss.doctype.label_printer.label_printer.download_label"  
-		    + "?label_reference=" + encodeURIComponent(label_printer)
-		    + "&content=" + encodeURIComponent(content))
-    );
-    if (!w) {
-        frappe.msgprint(__("Please enable pop-ups")); return;
-    }
-}
-
-function get_label_content(frm, dimensions) {
     var item_name = frm.doc.item_name;
     if ((item_name != frm.doc.item_purchasing_name)&&(frm.doc.item_purchasing_name)) {
         var item_name = frm.doc.item_purchasing_name;
@@ -123,52 +109,46 @@ function get_label_content(frm, dimensions) {
         var supplier_part_no = frm.doc.supplier_items[i].supplier_part_no
         supplier_item_entries += `${supplier}: ${supplier_part_no}<br>`
     }
-    
-    var content = `
-        <div style="width: ${dimensions.width}%; position: absolute; height: ${dimensions.height}px;">
-            <div style="position: absolute; z-index: 1;">
-                <div style="padding-top: 10px; font-size: ${dimensions.large_font_size}pt;">
-                    <b>${item_name}</b>
-                </div>
-                <div style="padding-top: 10px; font-size: ${dimensions.small_font_size}pt;">
-                    ${supplier_item_entries}
-                </div>
-                <div style="padding-top: 10px; font-size: ${dimensions.large_font_size}pt;">
-                    <b>${item_code}</b>
-                </div>
-            </div>
-            <div style="position: absolute; top: 0px; left: 0px; z-index: 2; min-width: 100%; min-height: 100%;background-image: url('https://data.libracore.ch/phpqrcode/api/qrcode.php?content=${url}&ecc=H&size=6&frame=2'); background-repeat: no-repeat; background-position: right bottom; background-size: 20%;">
-            </div>
-        </div>
-    `
-    return content;
-}
 
-function get_dimensions(label_printer) {
-    var dimensions = {
-        'Labels 106x48mm': {
-            width: 98,
-            height: 220,
-            large_font_size: 14,
-            small_font_size: 10
+    frappe.call({
+        'method': "energielenker.energielenker.utils.utils.get_label_dimension_settings",
+        'args': {
+            'label_printer': label_printer
         },
-        'Labels 60x30mm': {
-            width: 97,
-            height: 140,
-            large_font_size: 8,
-            small_font_size: 6
-        },
-        'Labels 101.6x38.1': {
-            width: 98,
-            height: 180,
-            large_font_size: 14,
-            small_font_size: 10
+        'async': false,
+        'callback': function(response) {
+            var dimensions = JSON.parse(response.message);
+            console.log(dimensions);
+            if (!dimensions[label_printer]) {
+                frappe.throw("Für dieses Etikett wurde kein Format hinterlegt.");
+            } else {
+                var dimensions = dimensions[label_printer];
+                var content = `
+                    <div style="width: ${dimensions.width}%; position: absolute; height: ${dimensions.height}px;">
+                        <div style="position: absolute; z-index: 1;">
+                            <div style="padding-top: 10px; font-size: ${dimensions.large_font_size}pt;">
+                                <b>${item_name}</b>
+                            </div>
+                            <div style="padding-top: 10px; font-size: ${dimensions.small_font_size}pt;">
+                                ${supplier_item_entries}
+                            </div>
+                            <div style="padding-top: 10px; font-size: ${dimensions.large_font_size}pt;">
+                                <b>${item_code}</b>
+                            </div>
+                        </div>
+                        <div style="position: absolute; top: 0px; left: 0px; z-index: 2; min-width: 100%; min-height: 100%;background-image: url('https://data.libracore.ch/phpqrcode/api/qrcode.php?content=${url}&ecc=H&size=6&frame=2'); background-repeat: no-repeat; background-position: right bottom; background-size: 20%;">
+                        </div>
+                    </div>
+                `
+                var w = window.open(
+                    frappe.urllib.get_full_url("/api/method/erpnextswiss.erpnextswiss.doctype.label_printer.label_printer.download_label"  
+                       + "?label_reference=" + encodeURIComponent(label_printer)
+                       + "&content=" + encodeURIComponent(content))
+               );
+               if (!w) {
+                   frappe.msgprint(__("Please enable pop-ups")); return;
+               }
+            }
         }
-    }
-
-    if (!dimensions[label_printer]) {
-        frappe.throw("Für dieses Etikett wurde kein Format hinterlegt.");
-    } else {
-        return dimensions[label_printer]
-    }
+    });
 }
--- energielenker/energielenker/utils/utils.py
+++ energielenker/energielenker/utils/utils.py
@@ -7,17 +7,23 @@ import re
 from erpnext.stock.reorder_item import reorder_item
 
 def get_plz_gebiet(self, event):
-	if not self.gebiet and self.customer_address:
-		_gebiet = frappe.db.get_value("Address", self.customer_address, "plz")
-		if _gebiet:
-			gebiet = re.findall(r"[0-9]{2,}", _gebiet)
-			if len(gebiet) > 0:
-				self.gebiet = gebiet[0][:2]
-	return
+    if not self.gebiet and self.customer_address:
+        _gebiet = frappe.db.get_value("Address", self.customer_address, "plz")
+        if _gebiet:
+            gebiet = re.findall(r"[0-9]{2,}", _gebiet)
+            if len(gebiet) > 0:
+                self.gebiet = gebiet[0][:2]
+    return
 
 @frappe.whitelist()	
 def reorder_item_wrapper():
-	reorder_item()
-	return
-		
-	
+    reorder_item()
+    return
+
+@frappe.whitelist()
+def get_label_dimension_settings(label_printer):
+    if frappe.db.exists("Label Printer", label_printer):
+        dimension_settings = frappe.db.get_value("Label Printer", label_printer, 'dimension_settings')
+        return dimension_settings
+    else:
+        return False
