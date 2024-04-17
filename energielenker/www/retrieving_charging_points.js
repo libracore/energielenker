024a2e5fd250e3b186b575fc0c9f61a7203c549e
Ivanios <ivan.lochbihler@libracore.com>
2024-04-17 14:36:00 +0200

    logic and process

--- energielenker/www/retrieving_charging_points.js
+++ energielenker/www/retrieving_charging_points.js
@@ -1,9 +1,18 @@
 frappe.ready(function() {
     $(".retrieving").toggle(true);
-    $("#create_license_key").click(function(){
-        var qty = $("#evse_count");
-        console.log(qty);
-        validate_qty(qty);
+    $(".success").toggle(false);
+    $(".retrieving").on("submit", function(event){
+        event.preventDefault();
+        var qty = $("#evse_count").val();
+        if (!qty) {
+            show_error('Bitte geben Sie eine Anzahl an.')
+            return false;
+        } else if (isNaN(qty) || !Number.isInteger(parseFloat(qty))) {
+            show_error("Bitte geben Sie eine ganze Zahl an.");
+            return false;
+        } else {
+            validate_qty(qty);
+        }
     });
 });
 
@@ -11,18 +20,30 @@ function validate_qty(qty) {
     frappe.call({
         'method': 'energielenker.www.retrieving_charging_points.validate_qty',
         'args': {
-            'qty': qty
+            'qty_string': qty
         },
         'callback': function(response) {
-            console.log(response.message);
             var validation = response.message;
             if (validation) {
-                console.log("shit");
-                window.location.href = 'charging_points_success';
+                if (validation == "Error") {
+                    show_error("Ein Fehler ist aufgetreten.");
+                } else {
+                    $("#license_key").val(validation);
+                    console.log(qty);
+                    $("#success_evse_count").text(qty);
+                    $(".retrieving").toggle(false);
+                    $(".success").toggle(true);
+                }
             } else {
-                console.log("peace");
-                $("#qty_error").show();
+                show_error("Verfügbare Menge überschritten.");
             }
         }
     });
 }
+
+function show_error(error) {
+    $("#form_error").html(`<i class="fa fa-exclamation-triangle" aria-hidden="true"></i> ${error}`);
+    $("#form_error").show();
+    $("#evse_count").css("border-color", "red");
+}
+
--- energielenker/www/retrieving_charging_points.py
+++ energielenker/www/retrieving_charging_points.py
@@ -3,6 +3,7 @@
 
 from __future__ import unicode_literals
 import frappe
+from frappe.utils.data import getdate
 
 no_cache = True
 
@@ -31,9 +32,14 @@ def get_ladepunkte(user):
     return avaliable_points
     
 @frappe.whitelist()
-def validate_qty(qty):
+def validate_qty(qty_string):
+    qty = int(qty_string)
+    uom_check = get_item_uom(qty)
+    if not uom_check:
+        license_key = "Error"
+        return license_key
     avaliable_points = get_ladepunkte(frappe.session.user)
-    if avaliable_points > qty:
+    if avaliable_points >= qty:
         license_key = create_license_key(qty)
         return license_key
     else:
@@ -41,72 +47,82 @@ def validate_qty(qty):
         
 def create_license_key(qty):
     purchase_order = create_purchase_order(qty)
-    license_key = create_lizenzgutscheine(purchase_order, qty)
-    # ~ log_entry = create_log_entry(license_key, qty)
+    lizenzgutschein = create_lizenzgutschein(purchase_order, qty)
+    license_key = get_license_key(lizenzgutschein)
+    log_entry = create_log_entry(license_key, qty)
     return license_key
     
 
-def create_purchase_order(sales_order_name):
-    # ~ #get today
-    # ~ today = getdate()
+def create_purchase_order(qty):
+    #get today
+    today = getdate()
     
-    # ~ #get Sales Order and settings
-    # ~ sales_order_doc = frappe.get_doc('Sales Order', sales_order_name)
-    # ~ po_settings = frappe.get_doc('Ladepunkt Key API Purchase Order', 'Ladepunkt Key API Purchase Order')
+    #get Sales Order and settings
+    po_settings = frappe.get_doc('Ladepunkt Key API Purchase Order', 'Ladepunkt Key API Purchase Order')
     
-    # ~ #create new Purchase Order
-    # ~ new_po_doc = frappe.get_doc({
-        # ~ 'doctype': 'Purchase Order',
-        # ~ 'supplier': po_settings.supplier,
-        # ~ 'schedule_date': today,
-        # ~ 'voraussichtlicher_liefertermin': today,
-        # ~ 'sales_order': sales_order_doc.name,
-        # ~ 'shipping_address_name': "",
-        # ~ 'shipping_address': "",
-        # ~ 'ansprechpartner': po_settings.ansprechpartner,
-        # ~ 'k_ansprechperson': po_settings.k_ansprechperson
-        # ~ })
+    #create new Purchase Order
+    new_po_doc = frappe.get_doc({
+        'doctype': 'Purchase Order',
+        'supplier': po_settings.supplier,
+        'schedule_date': today,
+        'voraussichtlicher_liefertermin': today,
+        'shipping_address_name': "",
+        'shipping_address': "",
+        'ansprechpartner': po_settings.ansprechpartner,
+        'k_ansprechperson': po_settings.k_ansprechperson
+        })
     
-    # ~ for item in sales_order_doc.items:
-        # ~ entry = {
-            # ~ 'reference_doctype': 'Purchase Order Item',
-            # ~ 'item_code': item.item_code,
-            # ~ 'schedule_date': today,
-            # ~ 'item_name': item.item_name,
-            # ~ 'qty': item.qty,
-            # ~ 'uom': item.uom,
-            # ~ 'sales_order': sales_order_doc.name,
-            # ~ 'sales_order_item': item.name,
-            # ~ 'cost_center': po_settings.cost_center
-        # ~ }
-        # ~ new_po_doc.append('items', entry)
+    entry = {
+        'reference_doctype': 'Purchase Order Item',
+        'item_code': po_settings.item_code,
+        'schedule_date': today,
+        'item_name': po_settings.item_name,
+        'qty': 1,
+        'uom': get_item_uom(qty),
+        'cost_center': po_settings.cost_center
+    }
+    new_po_doc.append('items', entry)
     
-    # ~ new_po_doc = new_po_doc.insert(ignore_permissions=True)
-    # ~ new_po_doc.submit()
+    new_po_doc = new_po_doc.insert(ignore_permissions=True)
+    new_po_doc.submit()
     
-    # ~ #get name of new Purchase Order and return it
-    # ~ purchase_order = new_po_doc.name
+    #get name of new Purchase Order and return it
+    purchase_order = new_po_doc.name
     
-    return "BE0001669-1"
+    return purchase_order
 
-def create_lizenzgutscheine(purchase_order_name, qty):
-    #get Purchase Order
-    # ~ purchase_order_doc = frappe.get_doc('Purchase Order', purchase_order_name)
+def create_lizenzgutschein(purchase_order_name, qty):
+    # ~ #get Purchase Order
+    purchase_order_doc = frappe.get_doc('Purchase Order', purchase_order_name)
     
-    # ~ lizenzgutschein = frappe.get_doc({
-        # ~ 'doctype': 'Lizenzgutschein',
-        # ~ 'purchase_order': purchase_order_doc.name,
-        # ~ 'positions_nummer': "1.1",
-        # ~ 'position_id': purchase_order_doc.items[0].name,
-        # ~ 'evse_count': qty
-        # ~ })
+    lizenzgutschein = frappe.get_doc({
+        'doctype': 'Lizenzgutschein',
+        'purchase_order': purchase_order_doc.name,
+        'positions_nummer': "1.1",
+        'position_id': purchase_order_doc.items[0].name,
+        'evse_count': qty
+        })
 
-    # ~ lizenzgutschein = lizenzgutschein.insert(ignore_permissions=True)
+    lizenzgutschein = lizenzgutschein.insert(ignore_permissions=True)
                 
-    lizenzgutschein = "Maschineeeee"
-    return lizenzgutschein
+    return lizenzgutschein.name
+    
+def get_license_key(lizenzgutschein):
+    data = frappe.db.sql("""SELECT
+                            `lizenzgutschein`
+                            FROM `tabLizenzgutschein`
+                            WHERE `name` = '{lg}'""".format(lg=lizenzgutschein), as_dict=True)
+                            
+    if len(data) > 0:
+        license_key = data[0].get('lizenzgutschein')
+    else:
+        frappe.log_error("Webshop Lizenzgutschein Error", "Lizenzgutschein Error")
+        license_key = "Error"
+        
+    return license_key
 
 def create_log_entry(license_key, qty):
+    today = getdate()
     customer = frappe.db.sql("""SELECT `parent` FROM `tabLicense Key Account User`  WHERE `user` = '{user}'""".format(user=frappe.session.user), as_dict=True)
     customer_doc = frappe.get_doc("License Key Account", customer[0].parent)
     
@@ -118,5 +134,16 @@ def create_log_entry(license_key, qty):
         'user': frappe.session.user
     }
     customer_doc.append('past_activities', entry)
+    customer_doc.save()
+    frappe.db.commit()
     
     return
+
+def get_item_uom(qty):
+    data = frappe.db.sql("""SELECT `name` FROM `tabUOM` WHERE `evse_count` = '{qty}'""".format(qty=qty), as_dict=True)
+    if len(data) == 1:
+        uom = data[0].get('name')
+        return uom
+    else:
+        frappe.log_error("Webshop UOM Error", "UOM Error")
+        return False
