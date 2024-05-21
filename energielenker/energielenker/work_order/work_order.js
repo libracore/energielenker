dc65b75577164f5f0ecf7f689e6956b28b105b01
Ivanios <ivan.lochbihler@libracore.com>
2024-05-21 17:46:09 +0200

    initialized print format for depot

--- energielenker/energielenker/work_order/work_order.js
+++ energielenker/energielenker/work_order/work_order.js
@@ -3,7 +3,6 @@
 
 frappe.ui.form.on('Work Order', {
     refresh: function() {
-        console.log(cur_frm.doc.project);
         cur_frm.fields_dict['sales_order'].get_query = function() {
             if (cur_frm.doc.project) {
                  return {
