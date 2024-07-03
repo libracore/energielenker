12bfa4677d8b34767d4bd660041878a6e78cd9af
Ivanios <ivan.lochbihler@libracore.com>
2024-07-03 18:03:17 +0200

    bugfix

--- energielenker/energielenker/report/sales_overview/sales_overview.py
+++ energielenker/energielenker/report/sales_overview/sales_overview.py
@@ -33,50 +33,52 @@ def get_data(filters):
                                         `tabSales Overview Item`""", as_dict=True)
     
     #get information for every item to consider and append it to data
-    for required_item in required_items:
-        #check if there is a lot calculation is required
-        if required_item.get('lot_calculation') == 1:
-            #get all entries
-            details = frappe.db.sql("""
-                                    SELECT
-                                        `item_code`,
-                                        `item_name`,
-                                        `uom`,
-                                        `qty`,
-                                        `amount`
-                                        
-                                    FROM
-                                        `tabSales Order Item`
-                                    WHERE
-                                        `item_code` = '{item}'
-                                    AND
-                                        `transaction_date` BETWEEN '{from_date}' AND '{to_date}'
-                                    AND
-                                        `docstatus` = 1""".format(item=required_item.get('item_code'), from_date=filters.from_date, to_date=filters.to_date), as_dict=True)
-            
-            #loop through items and calculate quantity
-            remove_sequence = "er-Los"
-            total_quantity = 0
-            total_amount = 0
-            for detail in details:
-                #If uom is piece, just add the quantity to the total
-                if detail.get('uom') == "Stück":
-                    total_quantity += detail.get('qty')
-                    total_amount += detail.get('amount')
-                #If there is a lot, remove the "er-los", convert to int, multiply with qty and add it to the total
-                else:
-                    quantity = int(detail.get('uom')[:-len(remove_sequence)]) * detail.get('qty')
-                    total_quantity += quantity
-                    total_amount += detail.get('amount')
-            
-            #create entry and append it to data
-            description = detail.get('item_code') + " " + detail.get('item_name')
-            entry = {
-                        'description': description,
-                        'quantity': total_quantity,
-                        'net_amount' : total_amount
-                    }
+    if len(required_items) > 0:
+        for required_item in required_items:
+            #check if there is a lot calculation is required
+            if required_item.get('lot_calculation') == 1:
+                #get all entries
+                details = frappe.db.sql("""
+                                        SELECT
+                                            `item_code`,
+                                            `item_name`,
+                                            `uom`,
+                                            `qty`,
+                                            `amount`
+                                            
+                                        FROM
+                                            `tabSales Order Item`
+                                        WHERE
+                                            `item_code` = '{item}'
+                                        AND
+                                            `transaction_date` BETWEEN '{from_date}' AND '{to_date}'
+                                        AND
+                                            `docstatus` = 1""".format(item=required_item.get('item_code'), from_date=filters.from_date, to_date=filters.to_date), as_dict=True)
+                
+                #loop through items and calculate quantity
+                remove_sequence = "er-Los"
+                total_quantity = 0
+                total_amount = 0
+                if len(details) > 0:
+                    for detail in details:
+                        #If uom is piece, just add the quantity to the total
+                        if detail.get('uom') == "Stück":
+                            total_quantity += detail.get('qty')
+                            total_amount += detail.get('amount')
+                        #If there is a lot, remove the "er-los", convert to int, multiply with qty and add it to the total
+                        else:
+                            quantity = int(detail.get('uom')[:-len(remove_sequence)]) * detail.get('qty')
+                            total_quantity += quantity
+                            total_amount += detail.get('amount')
                     
-            data.append(entry)
+                    #create entry and append it to data
+                    description = detail.get('item_code') + " " + detail.get('item_name')
+                    entry = {
+                                'description': description,
+                                'quantity': total_quantity,
+                                'net_amount' : total_amount
+                            }
+                            
+                    data.append(entry)
                 
     return data
