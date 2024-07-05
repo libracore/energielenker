d65549c1f414a61725db56a0328f67f4c1718142
Ivanios <ivan.lochbihler@libracore.com>
2024-07-05 10:59:59 +0200

    changed check from sales order to purchase order

--- energielenker/energielenker/purchase_invoice/purchase_invoice.py
+++ energielenker/energielenker/purchase_invoice/purchase_invoice.py
@@ -12,23 +12,25 @@ def validate_lagerfuehrung_of_items(purchase_invoice, event):
 
 @frappe.whitelist()
 def check_for_streckengeschaeft(doc_):
-	doc=json.loads(doc_)
-	#set default to non-streckengeschaeft
-	update_stock = 1
-	
-	#check if one item is streckengeschaeft
-	for item in doc['items']:
-		if item.get('purchase_order'):
-			so_item = frappe.db.sql("""
-				SELECT `soitem`.`delivered_by_supplier`
-				FROM `tabSales Order Item` AS `soitem`
-				LEFT JOIN `tabPurchase Order Item` AS `poitem` ON `poitem`.`sales_order_item` = `soitem`.`name`
-				WHERE `poitem`.`name` = '{po_detail}'
-				AND `poitem`.`docstatus` = 1
-				AND `soitem`.`docstatus` = 1""".format(po_detail=item['po_detail']), as_dict=True)
-			#if one item is streckengeschaeft, dont update stock - discussed with MR. Ruhkamp by call
-			if len(so_item) > 0:
-				if so_item[0]['delivered_by_supplier'] == 1:
-					update_stock = 0
-				
-	return update_stock
+    doc=json.loads(doc_)
+    #set default to non-streckengeschaeft
+    update_stock = 1
+    
+    #check if one item is streckengeschaeft
+    for item in doc['items']:
+        if item.get('purchase_order'):
+            po_item = frappe.db.sql("""
+                                    SELECT
+                                        `delivered_by_supplier`
+                                    FROM
+                                        `tabPurchase Order Item`
+                                    WHERE
+                                        `name` = '{po_detail}'
+                                    AND
+                                        `docstatus` = 1""".format(po_detail=item['po_detail']), as_dict=True)
+            #if one item is streckengeschaeft, dont update stock - discussed with MR. Ruhkamp by call
+            if len(po_item) > 0:
+                if po_item[0]['delivered_by_supplier'] == 1:
+                    update_stock = 0
+                
+    return update_stock
