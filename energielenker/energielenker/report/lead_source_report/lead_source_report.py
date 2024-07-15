36b25fd580ec95d525b81cc852660fd2653c3d39
Ivanios <ivan.lochbihler@libracore.com>
2024-07-15 13:35:48 +0200

    finished report

--- energielenker/energielenker/report/lead_source_report/lead_source_report.py
+++ energielenker/energielenker/report/lead_source_report/lead_source_report.py
@@ -17,30 +17,41 @@ def get_columns():
         {"label": _("Sales Order"), "fieldname": "sales_order", "fieldtype": "Link", "options": "Sales Order", "width": 100},
         {"label": _("Lead Source"), "fieldname": "lead_source", "fieldtype": "Data", "width": 100},
         {"label": _("Sales Order Date"), "fieldname": "sales_order_date", "fieldtype": "Date", "width": 100},
-        {"label": _("Customer Name"), "fieldname": "customer_name", "fieldtype": "Data", "width": 100},
+        {"label": _("Customer Name"), "fieldname": "customer_name", "fieldtype": "Data", "width": 150},
         {"label": _("PLZ"), "fieldname": "plz", "fieldtype": "Data", "width": 60},
-        {"label": _("Net Amount"), "fieldname": "net_amount", "fieldtype": "Data", "width": 100},
-        {"label": _("Customer Advisor (Sales Order)"), "fieldname": "customer_advisor", "fieldtype": "Data", "width": 100},
-        {"label": _("Customer Care (Customer)"), "fieldname": "customer_care", "fieldtype": "Data", "width": 100}
+        {"label": _("Net Amount"), "fieldname": "net_amount", "fieldtype": "Currency", "width": 100},
+        {"label": _("Customer Advisor (Sales Order)"), "fieldname": "customer_advisor", "fieldtype": "Data", "width": 150},
+        {"label": _("Customer Care (Customer)"), "fieldname": "customer_care", "fieldtype": "Data", "width": 150}
     ]
     return columns
 
 def get_data(filters):
     data = frappe.db.sql("""
-        SELECT
+        SELECT UNIQUE
             `so`.`name` AS `sales_order`,
             `so`.`transaction_date` AS `sales_order_date`,
             `so`.`customer` AS `customer_name`,
             `so`.`total` AS `net_amount`,
             `so`.`ansprechpartner` AS `customer_advisor`,
             `cust`.`ansprechpartner` AS `customer_care`,
-            `lead`.`source` AS `lead_source`
+            `lead`.`source` AS `lead_source`,
+            `addr`.`plz` AS `plz`
         FROM
             `tabSales Order` AS `so`
         LEFT JOIN
             `tabCustomer` AS `cust` ON `cust`.`name` = `so`.`customer`
         INNER JOIN
             `tabLead` AS `lead` ON `lead`.`name` = `cust`.`lead_name`
+        LEFT JOIN
+            `tabDynamic Link` AS `link`
+                ON `link`.`link_name` = `so`.`customer`
+        INNER JOIN
+            (SELECT
+                `name`, `plz`, `is_primary_address`
+            FROM
+                `tabAddress`
+            WHERE
+                `is_primary_address` = 1) AS `addr` ON `link`.`parent` = `addr`.`name`
         WHERE
             `so`.`docstatus` = 1
         AND
