0fc2b1b67b143f055a9d9a2f4418983cbaaa04e9
Ivanios <ivan.lochbihler@libracore.com>
2024-08-05 17:36:45 +0200

    finished task

--- energielenker/energielenker/purchase_order/purchase_order.py
+++ energielenker/energielenker/purchase_order/purchase_order.py
@@ -16,8 +16,10 @@ def get_default_cost_center(user):
     if fallback:
         return frappe.db.get_value("energielenker Settings", "energielenker Settings", "default_cost_center")
 
-def autoclose_purchase_order()
+def autoclose_purchase_order():
     affected_supplier = frappe.db.get_value("energielenker Settings", "energielenker Settings", "supplier_to_autoclose")
+    
+    autoclosed_documents = []
     
     orders = frappe.db.sql("""
                             SELECT
@@ -29,4 +31,26 @@ def autoclose_purchase_order()
                             AND
                                 `docstatus` = 1
                             AND
-                                `status` NOT IN ("Completed", "Closed", "On Hold")"""
+                                `status` IN ("To Receive and Bill", "To Receive", "To Bill")""".format(supplier=affected_supplier), as_dict=True)
+                                
+    for order in orders:
+        open_lizenzgutscheine = frappe.db.sql("""
+                                                SELECT
+                                                    `name`
+                                                FROM
+                                                    `tabLizenzgutschein`
+                                                WHERE
+                                                    `purchase_order` = '{po}'
+                                                AND
+                                                    `status` = 'Gültig'""".format(po=order.get('name')), as_dict=True)
+        
+        if not len(open_lizenzgutscheine) > 0:
+            po_doc = frappe.get_doc("Purchase Order", order.get('name'))
+            po_doc.set_status(update=True, status="Closed")
+            po_doc.save()
+            frappe.db.commit()
+            autoclosed_documents.append(order.get('name'))
+            
+    frappe.log_error(autoclosed_documents, "Closed Purchase Orders")
+            
+    return
