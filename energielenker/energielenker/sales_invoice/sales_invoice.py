4b3e8069c9814f24f11e9c168d2aad5e4fcbbb78
Ivanios <ivan.lochbihler@libracore.com>
2024-08-07 13:21:45 +0200

    finished sales order

--- energielenker/energielenker/sales_invoice/sales_invoice.py
+++ energielenker/energielenker/sales_invoice/sales_invoice.py
@@ -22,16 +22,36 @@ def charged_at_cost(self, event):
     #get all sales orders which are affected
     affected_sales_orders = []
     for si_item in self.get('items'):
-        if si_item.get('artikel_nach_aufwand') and si_item.get('artikel_nach_aufwand') not in affected_sales_orders:
+        if si_item.get('enthaelt_artikel_nach_aufwand') and si_item.get('sales_order') not in affected_sales_orders:
             affected_sales_orders.append(si_item.get('sales_order'))
     
     for sales_order in affected_sales_orders:
         sales_order_doc = frappe.get_doc("Sales Order", sales_order)
         
-            
-    frappe.db.set_value("Sales Order", "AB0001479", "per_billed", 100)
-    frappe.db.set_value("Sales Order", "AB0001479", "status", "To Deliver")
+        invoices = frappe.db.sql("""
+                                    SELECT
+                                        SUM(`qty`) as `qty`
+                                    FROM
+                                        `tabSales Invoice Item`
+                                    WHERE
+                                        `sales_order` = '{so}'
+                                    AND
+                                        `docstatus` = 1""".format(so=sales_order), as_dict=True)
+        frappe.log_error(invoices[0].qty, "invoices[0].qty")
+        frappe.log_error(sales_order_doc.get('total_qty'), "sales_order_doc.get('total_qty')")
+        per_billed = invoices[0].qty / sales_order_doc.get('total_qty') * 100
+        
+        frappe.db.set_value("Sales Order", sales_order_doc.get('name'), "per_billed", per_billed)
+        frappe.db.set_value("Sales Order", sales_order_doc.get('name'), "set_with_quantity", 1)
+        
+        if sales_order_doc.status == "To Deliver and Bill" and per_billed == 100:
+            frappe.db.set_value("Sales Order", sales_order_doc.get('name'), "status", "To Deliver")
+        elif sales_order_doc.status == "To Bill" and per_billed == 100:
+            frappe.db.set_value("Sales Order", sales_order_doc.get('name'), "status", "Completed")
+        elif sales_order_doc.status == "To Deliver" and per_billed < 100:
+            frappe.db.set_value("Sales Order", sales_order_doc.get('name'), "status", "To Deliver and Bill")
+        elif sales_order_doc.status == "Completed" and per_billed < 100:
+            frappe.db.set_value("Sales Order", sales_order_doc.get('name'), "status", "To Bill")
             
     return
-
 
