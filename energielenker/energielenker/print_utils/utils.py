0b1e811862d22161cfa51a26838893b5c9f7e2ec
Ivanios <ivan.lochbihler@libracore.com>
2024-10-07 15:34:38 +0200

    added clear contact to quotation sales order and invoice

--- energielenker/energielenker/print_utils/utils.py
+++ energielenker/energielenker/print_utils/utils.py
@@ -1194,7 +1194,7 @@ def get_print_items(dt, dn, total_value_needed=False):
         # Abschlagspositionen
         total_anzahlung = 0
         total_anzahlung_mwst = 0
-        if len(sales_order.billing_overview) > 0:
+        if len(sales_order.billing_overview) > 1:
             tr = """
                 <tr style="background-color: rgb(186, 210, 226) !important;">
                     <td style="text-align: center;"><b>{position}</b></td>
@@ -1358,7 +1358,7 @@ def get_print_items(dt, dn, total_value_needed=False):
         total_brutto = "{:,.2f}".format(doc.grand_total - total_anzahlung - total_anzahlung_mwst).replace(",", "'").replace(".", ",").replace("'", ".")
         total_anzahlung = "{:,.2f}".format(total_anzahlung).replace(",", "'").replace(".", ",").replace("'", ".")
         total_anzahlung_mwst = "{:,.2f}".format(total_anzahlung_mwst).replace(",", "'").replace(".", ",").replace("'", ".")
-        
+        frappe.log_error(total_brutto, "total_brutto")
         tr = """
             <tr class="blue-white">
                 <td colspan="2" style="width: 50% ; background-color: white !important;"></td>
