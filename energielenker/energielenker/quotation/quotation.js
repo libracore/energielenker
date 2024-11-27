0112d526bab191e58ea732ba0a4d6e8c3b953b75
Ivanios <ivan.lochbihler@libracore.com>
2024-11-27 17:01:09 +0100

    removed item type part list, if necessary

--- energielenker/energielenker/quotation/quotation.js
+++ energielenker/energielenker/quotation/quotation.js
@@ -140,8 +140,12 @@ frappe.ui.form.on('Quotation Item', {
             frappe.db.get_value("Item", row.item_code, "part_list_item").then( (value) => {
                if (value.message.part_list_item == 1) {
                     frappe.model.set_value(cdt, cdn, 'with_bom', 1);
+                } else {
+                    frappe.model.set_value(cdt, cdn, 'with_bom', 0);
                 }
             });
+        } else {
+            frappe.model.set_value(cdt, cdn, 'with_bom', 0);
         }
     },
     before_items_remove(frm, cdt, cdn) {
