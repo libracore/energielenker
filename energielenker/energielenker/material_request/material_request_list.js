402d11deebacaa3950b6ec9c559ebe0896ba1e0a
Ivanios <ivan.lochbihler@libracore.com>
2025-02-12 09:45:45 +0100

    added status to list view

--- energielenker/energielenker/material_request/material_request_list.js
+++ energielenker/energielenker/material_request/material_request_list.js
@@ -1,4 +1,27 @@
 frappe.listview_settings['Material Request'] = {
+    get_indicator: function(doc) {
+        if (doc.status== "Stopped") {
+            return [__("Stopped"), "red", "status,=," + "Stopped"]
+        
+        } else if (doc.status== "Partially Ordered") {
+            return [__("Partially Ordered"), "yellow", "status,=," + "Partially Ordered"]
+        
+        } else if (doc.status== "Ordered") {
+            return [__("Ordered"), "green", "status,=," + "Ordered"]
+        
+        } else if (doc.status== "Issued") {
+            return [__("Issued"), "green", "status,=," + "Issued"]
+        
+        } else if (doc.status== "Transferred") {
+            return [__("Transferred"), "green", "status,=," + "Transferred"]
+        
+        } else if (doc.status== "Received") {
+            return [__("Received"), "green", "status,=," + "Received"]
+        
+        } else if (doc.status== "Pending") {
+            return [__("Pending"), "grey", "status,=," + "Pending"]
+        }
+    },
     onload: function(listview) {
         listview.page.add_menu_item( __("Autocreate Material Request"), function() {
           autocreate_material_requests();
