808f7423c7cd44ff6572617287e62eff8f1f3ff2
Ivanios <ivan.lochbihler@libracore.com>
2025-07-23 20:17:57 +0200

    customizations - started patch

---
+++ energielenker/patches/v2_147_0/submit_zoho_data.py
@@ -0,0 +1,34 @@
+import frappe
+from frappe.modules.utils import sync_customizations
+
+def execute():
+    sync_customizations("energielenker")
+    
+    affected_issues = frappe.db.sql("""
+                                    SELECT
+                                        `name`
+                                    FROM
+                                        `tabIssue`""", as_dict=True)
+    
+    for issue in affected_issues:
+        sql_query = """
+            SELECT SUM(`hours`) AS `total_hours`
+            FROM `tabTimesheet Detail`
+            WHERE `issue` = '{issue}'
+            AND `docstatus` = 1;""".format(issue=issue.get('name'))
+    
+        total_hours = frappe.db.sql(sql_query, as_dict=True)
+
+        if len(total_hours) > 0:
+            booked_hours = total_hours[0].total_hours
+        else:
+            booked_hours = 0
+        
+        if not booked_hours:
+            booked_hours = 0
+        
+        frappe.db.set_value("Issue", issue, "booked_hours", booked_hours)
+    
+    frappe.db.commit()
+
+    return
