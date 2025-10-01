b356e076c3770f5656df65199b532655d6672d78
Ivanios <ivan.lochbihler@libracore.com>
2025-10-01 12:49:22 +0200

    updated dn assigment process

---
+++ energielenker/energielenker/doctype/delivery_note_assignment_users/delivery_note_assignment_users.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, libracore and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Delivery Note Assignment Users', {
+	// refresh: function(frm) {
+
+	// }
+});
