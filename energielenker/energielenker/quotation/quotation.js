ba7ecc614e6d055a99cf15d858d61c785ad7d162
Ivanios <ivan.lochbihler@libracore.com>
2025-10-15 11:33:55 +0200

    removed produktionsstandard from addres queries

--- energielenker/energielenker/quotation/quotation.js
+++ energielenker/energielenker/quotation/quotation.js
@@ -116,7 +116,6 @@ frappe.ui.form.on('Quotation', {
     },
     party_name: function(frm) {        
         if (cur_frm.doc.quotation_to == 'Customer') {
-            setTimeout(function(){ shipping_address_query(frm); }, 500);
             check_foreign_customers(frm.doc.party_name);
         }
         
@@ -281,19 +280,6 @@ function set_item_typ(item) {
         }
     }
     cur_frm.refresh_field('items');
-}
-
-function shipping_address_query(frm) {
-    cur_frm.fields_dict['shipping_address_name'].get_query = function(doc) {
-        return {
-            query: 'frappe.contacts.doctype.address.address.address_query',
-            filters: {
-                'link_doctype': cur_frm.doc.quotation_to,
-                'link_name': cur_frm.doc.party_name,
-                'produktionsstandort': 1
-            }
-        }
-    };
 }
 
 function cost_center_query(frm) {
