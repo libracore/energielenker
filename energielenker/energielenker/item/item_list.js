bc57ab5295ad3b14f182906cb0e691b1c135766a
Ivanios <ivan.lochbihler@libracore.com>
2025-10-15 11:20:40 +0200

    removed 500 entries from item

--- energielenker/energielenker/item/item_list.js
+++ energielenker/energielenker/item/item_list.js
@@ -1,6 +1,3 @@
 frappe.listview_settings['Item'] = {
-    onload: function(listview) {
-        var pg_button = document.getElementById('page-List/Item/List').getElementsByClassName("btn-paging")[2];
-        pg_button.click();
-    }
+    //Some exciting libracore Code here
 };
