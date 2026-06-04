212f6b45b9ece036869a6ae1a6608bafafb69a8f
Ivanios <ivan.lochbihler@libracore.com>
2026-06-04 11:16:51 +0200

    lines without sales order

--- energielenker/energielenker/report/service_projects/service_projects.py
+++ energielenker/energielenker/report/service_projects/service_projects.py
@@ -124,6 +124,7 @@ def get_invoiceable_entries(from_date=None, to_date=None, customer=None, project
             OR (DATE(`tabTimesheet Detail`.`to_time`) >= "{from_date}" AND DATE(`tabTimesheet Detail`.`to_time`) <= "{to_date}"))
            AND `tabTimesheet Detail`.`no_bill` = 0
            AND `tabTimesheet Detail`.`billed_with_service_project` = 0
+               AND `tabSales Order Item`.`parent` IS NOT NULL
            AND `tabProject`.`is_service_project` = 1
            AND (`tabSales Order Item`.`docstatus` != 2
             OR `tabSales Order Item`.`docstatus` IS NULL)
@@ -136,6 +137,7 @@ def get_invoiceable_entries(from_date=None, to_date=None, customer=None, project
         project_condition=project_condition
     )
     entries = frappe.db.sql(sql_query, as_dict=True)
+    frappe.log_error(entries, "entries")
     return entries
 
 @frappe.whitelist()
@@ -151,6 +153,7 @@ def create_invoice(from_date, to_date, project):
         if entry.get('sales_order') and entry.get('so_docstatus') != 1:
             frappe.throw("Kundenauftrag {0} muss zuerst gebucht werden.".format(entry.get('sales_order')))
         if entry.get('sales_order'):
+            frappe.log_error(entry, "entry")
             filtered_entries.append({
                                     'so_detail': entry.get('so_detail'),
                                     'remarks': entry.get('remarks'),
@@ -212,7 +215,7 @@ def create_invoice(from_date, to_date, project):
                 
         #Add Last item to Invoice
         invoice.append('items', new_item)
-        
+        frappe.log_error(invoice, "invoice")
         #Insert Invoice
         invoice.insert()
         
